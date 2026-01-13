1e85e99daa15be6eff0d2392b1fc436c6977e862
AfeefaAnsaM <afeefaansam@gmail.com>
2026-01-13 19:31:15 +0530

    Add files via upload

--- student.py
+++ student.py
@@ -29,23 +29,7 @@ class Student:
         cursor.execute("DELETE FROM students WHERE id=?", (id,))
         conn.commit()
         conn.close()
-        @staticmethod
-def update_student(id, name=None, branch=None, year=None):
-    conn = connect_db()
-    cursor = conn.cursor()
+        
     
-    # Update name if provided
-    if name:
-        cursor.execute("UPDATE students SET name=? WHERE id=?", (name, id))
-    
-    # Update branch if provided
-    if branch:
-        cursor.execute("UPDATE students SET branch=? WHERE id=?", (branch, id))
-    
-    # Update year if provided
-    if year:
-        cursor.execute("UPDATE students SET year=? WHERE id=?", (year, id))
-    
-    conn.commit()
-    conn.close()
+  
 
